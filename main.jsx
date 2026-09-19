77c214ec88113bf57717b2c296a0ae14c599740e
harshrajzala1296 <harshrajzala0369@gmail.com>
2026-09-19 21:00:17 +0530

    Keep only server backend in repository

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import KaamAI from './KaamAI.jsx';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <KaamAI />
-  </React.StrictMode>
-);
